16c9dedb63c6c77fd1a38ef999d9b2981d0e4faf
Charles Villard <charles.villard@epita.fr>
2024-02-12 15:18:48 +0100

    edit: exp: added hierarchical optimization

--- exp.py
+++ exp.py
@@ -43,24 +43,39 @@ def exec_(params, i):
     return process
 
 def verif(i):
-    micmacBascule(workingDir, refDir, "Ori-BestInit"+str(i)+"0")
-    return micmacCmp(workingDir, "tif", refDir, "Ori-BestInit"+str(i)+"0")
+    ori = "Ori-BestInit"+str(i)+"0"
+    if not os.path.isdir(ori):
+        return -1,-1
+
+    micmacBascule(workingDir, refDir, ori)
+    return micmacCmp(workingDir, "tif", refDir, ori)
+
+def verifH(i):
+    ori = "Ori-BestInit"+str(i)+"0Hierarchique"
+    if not os.path.isdir(ori):
+        return -1,-1
+
+    micmacBascule(workingDir, refDir, ori)
+    return micmacCmp(workingDir, "tif", refDir, ori)
+
 
 def f(cmd, j):
     start = time.time()
     p = exec_(cmd, j)
     p.communicate()
-    center, angles = verif(j)
+    center1, angles1 = verif(j)
+    center2, angles2 = verifH(j)
     end = time.time()
     elapsedtime = end - start
-    print("Center : " + str(center) + " Angle : " + str(angles) \
+    print("Center : " + str(center1) + " Angle : " + str(angles1) \
             + " on " + str(j) + " in " + str(elapsedtime))
-    return center, angles, elapsedtime
+    return center1, angles1, center2, angles2, elapsedtime
 
 def experiences(cmds, i = 0):
     print("Number of tests to try: " + str(len(cmds)) + " N times: " + str(i))
     csvfile = open('results.csv', 'w', newline='')
-    fieldnames = ['N', 'R0', 'Pond', 'DistCenter', 'DistAngle', 'Time']
+    fieldnames = ['N', 'R0', 'Pond', 'DistCenter', 'DistAngle', 'DistHCenter',
+                  'DistHAngle', 'Time']
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
     writer.writeheader()
     csv_writer_lock = threading.Lock()
@@ -72,14 +87,15 @@ def experiences(cmds, i = 0):
         with Pool(processes=10) as pool:
             multiple_results = [pool.apply_async(f, (cmds[c], j)) for j in range(i)]
             for res in multiple_results:
-                center, angles, elapsedtime = res.get(timeout=10000)
+                center1, angles1, center2, angles2, elapsedtime = res.get(timeout=10000)
                 with csv_writer_lock:
                     writer.writerow({'N': cmds[c][0], 'R0': cmds[c][1], 'Pond':
-                                 cmds[c][2], 'DistCenter': center, 'DistAngle':
-                                     angles, 'Time': elapsedtime})
+                                     cmds[c][2], 'DistCenter': center1, 'DistAngle':
+                                     angles1, 'DistHCenter': center2,
+                                     'DistHAngle': angles2, 'Time': elapsedtime})
                     csvfile.flush()
-                results_center.append(center)
-                results_angles.append(angles)
+                results_center.append(center1)
+                results_angles.append(angles1)
 
         print("-------------------------------")
         print("         Result for :          ")
