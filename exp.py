8528be26c368bba93a2d613c4f9ad1135eda33fc
Charles Villard <charles.villard@epita.fr>
2024-02-18 21:21:30 +0100

    edit: exp: estimate r1 and r2

--- exp.py
+++ exp.py
@@ -19,6 +19,8 @@ import threading
 
 nArg = len(sys.argv)
 refDir = sys.argv[1]
+imageExt = sys.argv[2]
+
 #testDir = sys.argv[2]
 
 path = Path(refDir)
@@ -28,13 +30,13 @@ print(workingDir)
 from basculed import *
 
 
-def exp(it, r0, pond, opti, i):
-    return ["mm3d", "TestLib", "NO_SolInit_RndForest", ".*.tif",
-            "OriCalib=CalibPerf", "SH=5Pts", "OriOut=BestInit" + str(i), "Nb=" +
-            str(it), "R0=" + str(r0), "Pond=" + str(pond), "Opti=" + str(opti)]
+def exp(it, r0, r1, pond, opti, i, oriname):
+    return ["mm3d", "TestLib", "NO_SolInit_RndForest", ".*."+str(imageExt),
+            "OriCalib=CalibPerf", "SH=5Pts", "OriOut=" + oriname, "Nb=" +
+            str(it), "R0=" + str(r0), "R1=" + str(r1), "Pond=" + str(pond), "Opti=" + str(opti)]
 
-def exec_(params, i):
-    cmd = exp(params[0], params[1], params[2], params[3], i)
+def exec_(params, oriname, i):
+    cmd = exp(params[0], params[1], params[2],  params[3], params[4], i, oriname)
 
     print(cmd)
     log = open("/dev/null", "w")
@@ -44,76 +46,79 @@ def exec_(params, i):
     #print(stdout)
     return process
 
-def verif(i):
-    ori = "Ori-BestInit"+str(i)+"0"
+def verif(oriname):
+    ori = "Ori-" + oriname+"0"
     if not os.path.isdir(ori):
         return -1,-1
 
     micmacBascule(workingDir, refDir, ori)
-    return micmacCmp(workingDir, "tif", refDir, ori)
+    return micmacCmp(workingDir, imageExt, refDir, ori)
 
-def verifH(i):
-    ori = "Ori-BestInit"+str(i)+"0Hierarchique"
+def verifH(oriname):
+    ori = "Ori-" + oriname+"0Hierarchique"
     if not os.path.isdir(ori):
         return -1,-1
 
     micmacBascule(workingDir, refDir, ori)
-    return micmacCmp(workingDir, "tif", refDir, ori)
+    return micmacCmp(workingDir, imageExt, refDir, ori)
 
 
 def f(cmd, j):
+    oriname = "Exp-N" + str(cmd[0]) + "-R" + str(cmd[1]) + \
+        "-R" + str(cmd[2]) + "-P" + str(cmd[3]) + "-" + str(j)
     start = time.time()
-    p = exec_(cmd, j)
+    p = exec_(cmd, oriname, j)
     p.communicate()
-    center1, angles1 = verif(j)
-    center2, angles2 = verifH(j)
+    center1, angles1 = verif(oriname)
+    center2, angles2 = verifH(oriname)
     end = time.time()
     elapsedtime = end - start
     print("Center : " + str(center1) + " Angle : " + str(angles1) \
             + " on " + str(j) + " in " + str(elapsedtime))
-    return center1, angles1, center2, angles2, elapsedtime
+    return cmd, center1, angles1, center2, angles2, elapsedtime
 
 def experiences(cmds, i = 0):
     print("Number of tests to try: " + str(len(cmds)) + " N times: " + str(i))
     csvfile = open('results.csv', 'w', newline='')
-    fieldnames = ['N', 'R0', 'Pond', 'DistCenter', 'DistAngle', 'DistHCenter',
+    fieldnames = ['N', 'R0', 'R1', 'Pond', 'DistCenter', 'DistAngle', 'DistHCenter',
                   'DistHAngle', 'Time']
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
     writer.writeheader()
     csv_writer_lock = threading.Lock()
 
-    processes = []
-    for c in range(len(cmds)):
-        results_center = []
-        results_angles = []
-        with Pool(processes=10) as pool:
-            multiple_results = [pool.apply_async(f, (cmds[c], j)) for j in range(i)]
-            for res in multiple_results:
-                center1, angles1, center2, angles2, elapsedtime = res.get(timeout=10000)
-                with csv_writer_lock:
-                    writer.writerow({'N': cmds[c][0], 'R0': cmds[c][1], 'Pond':
-                                     cmds[c][2], 'DistCenter': center1, 'DistAngle':
-                                     angles1, 'DistHCenter': center2,
-                                     'DistHAngle': angles2, 'Time': elapsedtime})
-                    csvfile.flush()
-                results_center.append(center1)
-                results_angles.append(angles1)
+    with Pool(processes=10) as pool:
+        multiple_results = [pool.apply_async(f, (cmds[j//i], j%i)) for j in
+                            range(len(cmds) * i)]
+        for res in multiple_results:
+            cmd, center1, angles1, center2, angles2, elapsedtime = res.get(timeout=10000)
+            with csv_writer_lock:
+                writer.writerow({'N': cmd[0],
+                                     'R0': cmd[1],
+                                     'R1': cmd[2],
+                                     'Pond': cmd[3],
+                                     'DistCenter': center1,
+                                     'DistAngle': angles1,
+                                     'DistHCenter': center2,
+                                     'DistHAngle': angles2,
+                                     'Time': elapsedtime})
+                csvfile.flush()
 
-        print("-------------------------------")
-        print("         Result for :          ")
-        print(cmds[c])
-        print("---")
-        print(results_center)
-        print(results_angles)
-        print("---")
-        print(str(np.mean(results_center)) + " - " + str(np.std(results_center)) + " - " + str(np.var(results_center)))
-        print(str(np.mean(results_angles)) + " - " + str(np.std(results_angles)) + " - " + str(np.var(results_angles)))
-        print("-------------------------------")
+#        print("-------------------------------")
+#        print("         Result for :          ")
+#        print(cmds[c])
+#        print("---")
+#        print(results_center)
+#        print(results_angles)
+#        print("---")
+#        print(str(np.mean(results_center)) + " - " + str(np.std(results_center)) + " - " + str(np.var(results_center)))
+#        print(str(np.mean(results_angles)) + " - " + str(np.std(results_angles)) + " - " + str(np.var(results_angles)))
+#        print("-------------------------------")
 
 
 listes_exp = []
-for n in np.arange(0.1, 2, 0.1):
-    listes_exp.append([500, n, 1, 0])
+for r0 in np.arange(0., 5, 0.2):
+    for r1 in np.arange(0., 5, 0.2):
+        listes_exp.append([1000, r0, r1, 1, 0])
 print(listes_exp)
 
-experiences(listes_exp, 10)
+experiences(listes_exp, 1)
